9dd363a1b00603da5e6d65295a8c37f3ec97f258
nilab-khwaja <nilab.khwajazada1212@gmail.com>
2023-10-10 15:20:39 +0430

    resolve linters

--- src/components/MakupDetail.jsx
+++ src/components/MakupDetail.jsx
@@ -5,8 +5,9 @@ import '../styles/makeupdetail.css';
 
 const MakupDetail = () => {
   const { produtId } = useParams();
-  const makeup = useSelector((state) => state.makeups.makeups.find((item) => 
-  item.id === parseInt(produtId, 10)));
+  const makeup = useSelector((state) => {
+    return state.makeups.makeups.find((item) => item.id === parseInt(produtId, 10));
+  });
 
   if (!makeup) {
     return <div>Product not found</div>;
